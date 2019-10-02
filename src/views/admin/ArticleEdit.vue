66bb3e8c16b9f1a89217dceabfa82692148a55eb
smallsixFight <smallsix.fight@gmail.com>
2019-10-02 15:06:13 +0800

    1. 调整左侧菜单栏； 2. 完善文章编辑页；

--- src/views/admin/ArticleEdit.vue
+++ src/views/admin/ArticleEdit.vue
@@ -1,139 +1,175 @@
 <template>
-    <div v-loading="loading">
-        <el-form :model="articleForm" :rules="rules" ref="articleForm">
-            <el-card class="page-header">
-                <span>文章编辑</span>
-            </el-card>
-            <el-card>
-                <div class="article-select">
-                    <el-row :gutter="24">
-                        <el-col :span="8">
-                            <el-form-item label="分类">
-                                <metaList type=2 :defaultVal='articleForm.category' v-on:change="handleCatoryChange" />
-                            </el-form-item>
-                        </el-col>
-                        <el-col :span="8">
-                            <el-form-item label="标签">
-                                <metaList type=1 :defaultVal='articleForm.tags' :multiple='true' v-on:change="handleTagsChange" />
-                            </el-form-item>
-                        </el-col>
-                        <el-col :span="8">
-                            <el-form-item label="开启评论">
-                                <el-select v-model="articleForm.allowComment" size="small">
-                                    <el-option :key=1 :value='1' label="是"></el-option>
-                                    <el-option :key=0 :value='0' label="否"></el-option>
-                                </el-select>
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                </div>
-                <el-form-item prop='title'>
-                    <el-input placeholder="输入文章标题" v-model="articleForm.title" class="article-title"></el-input>
-                </el-form-item>
-                <el-form-item prop="content">
-                    <markdownEdit :val='articleForm.content' v-on:change="handleContentChange"></markdownEdit>
-                </el-form-item>
-                <el-form-item>
-                    <div style="float: right;">
-                        <el-button class="editor-btn" size="small" type="warning" @click="submit('draft')">保存草稿</el-button>
-                        <el-button class="editor-btn" size="small" type="primary" @click="submit('publish')">发布</el-button>
-                    </div>
-                </el-form-item>
-                
-            </el-card>
-        </el-form>
-    </div>
+  <div v-loading="loading">
+    <el-form :model="articleForm">
+      <el-card class="page-header">
+        <span>文章编辑</span>
+      </el-card>
+      <el-card>
+        <div class="article-select">
+          <el-row :gutter="24">
+            <el-col :span="8">
+              <el-form-item label="分类">
+                <metaList
+                  type="2"
+                  :key="articleForm.article.id"
+                  :defaultVal="articleForm.category"
+                  v-on:change="handleCatoryChange"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="标签">
+                <metaList
+                  type="1"
+                  :key="articleForm.article.id"
+                  :defaultVal="articleForm.tag_list"
+                  :multiple="true"
+                  v-on:change="handleTagsChange"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="开启评论">
+                <el-select v-model="articleForm.article.allow_comment" size="small">
+                  <el-option :key="true" :value="true" label="是"></el-option>
+                  <el-option :key="false" :value="false" label="否"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+        <el-form-item prop="title">
+          <el-input placeholder="输入文章标题" v-model="articleForm.article.title" class="article-title"></el-input>
+        </el-form-item>
+        <el-form-item prop="content">
+          <markdownEdit
+            :key="articleForm.article.id"
+            :val="articleForm.article.content"
+            v-on:change="handleContentChange"
+          ></markdownEdit>
+        </el-form-item>
+        <el-form-item>
+          <div style="float: right;">
+            <el-button class="editor-btn" size="small" type="warning" @click="submit(0)">保存草稿</el-button>
+            <el-button class="editor-btn" size="small" type="primary" @click="submit(1)">发布</el-button>
+          </div>
+        </el-form-item>
+      </el-card>
+    </el-form>
+  </div>
 </template>
 
 <script>
-import markdownEdit from '../components/MarkdownEdit';
-import metaList from '../components/LookupMeta';
-import 'mavon-editor/dist/css/index.css';
-// import { stringify } from 'qs'
+import markdownEdit from "../components/MarkdownEdit";
+import metaList from "../components/LookupMeta";
 export default {
-    name: "articleEdit",
-    data() {
-        return {
-            loading: false,
-            articleForm: {
-                content: '',
-                title: '',
-                allowComment: 1,
-                category: '2',
-                tags: ['12'],
-                slug: ''
-            },
-            rules: {
-                 title: [
-                    { required: true, message: '请输入文章标题', trigger: 'blur' }
-                ],
-                content: [
-                    { required: true, message: '请输入文章内容', trigger: 'blur' }
-                ]
-            },
-            html:'',
-            categoryList: [],
-            tagList: []
-            
-        }
-    },
-    components: {
-        markdownEdit,
-        metaList
-    },
-    created: function() {
-        // const cid = this.$route.query.cid
-        // this.getInitalizeData(cid);
-    },
-    methods: {
-        handleContentChange(val) {
-            this.articleForm.content = val
+  name: "articleEdit",
+  data() {
+    return {
+      loading: false,
+      articleForm: {
+        article: {
+          content: "",
+          title: "",
+          allow_comment: true
         },
-        handleCatoryChange(val) {
-            this.articleForm.category = val;
-        },
-        handleTagsChange(val) {
-            this.articleForm.tags = val;
-        },
-        submit(status) {
-            console.log(this.articleForm.tags)
-            this.$refs['articleForm'].validate((valid) => {
-                if (!valid)
-                    return false
-                this.loading = true
-                this.articleForm.status = status
-                this.articleForm.tags = this.articleForm.tags ? this.articleForm.tags.join(',') : ''
-                this.$axios.post(this.HOST + "/admin/api/addOrUpdateArticle", {
-                    ...this.articleForm
-                }).then(response => {
-                    if (response.data.code === 0) {
-                        this.dialogFormVisible = false
-                        this.$router.replace('/admin/articleList')
-                        this.$message.success(response.data.message)
-                    } else {
-                        this.$message.warning(response.data.message)
-                    }
-                }).finally( () => {
-                    this.loading = false
-                })
-            })
-            
-        }
+        category: "",
+        tag_list: []
+      },
+      html: "",
+      categoryList: [],
+      tagList: []
+    };
+  },
+  components: {
+    markdownEdit,
+    metaList
+  },
+  created: function() {
+    const id = this.$route.query.id;
+    if (id && id !== 0) {
+      this.loading = true;
+      this.$axios
+        .get(this.HOST + "/admin/article/info/" + id)
+        .then(resp => {
+          if (resp.data.success) {
+            this.articleForm = resp.data.data;
+            console.log(this.articleForm.article);
+          } else {
+            this.$message.warning(resp.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
-}
+  },
+  methods: {
+    handleContentChange(val) {
+      this.articleForm.article.content = val;
+    },
+    handleCatoryChange(val) {
+      this.articleForm.category = val;
+    },
+    handleTagsChange(val) {
+      this.articleForm.tag_list = val;
+    },
+
+    // 更新或新增文章
+    async submit(status) {
+      if (this.articleForm.article.title.trim() === "") {
+        this.$message.warning("请输入标题");
+        return;
+      }
+      if (this.articleForm.article.content.trim() === "") {
+        this.$message.warning("请输入内容");
+        return;
+      }
+      if (this.articleForm.category === "") {
+        this.$message.warning("请至少选择一个分类");
+        return;
+      }
+
+      this.articleForm.article.status = status;
+      try {
+        this.loading = true;
+        let resp;
+        if (this.articleForm.article.id && this.articleForm.id !== "id") {
+          delete this.articleForm.article.create_time;
+          delete this.articleForm.article.modify_time;
+          resp = await this.$axios.put(this.HOST + "/admin/article/update", {
+            ...this.articleForm
+          });
+        } else {
+          resp = await this.$axios.post(this.HOST + "/admin/article/add", {
+            ...this.articleForm
+          });
+        }
+        if (resp.data.success) {
+          this.$message.success(resp.data.message);
+          this.$router.replace("/admin/articleList");
+        } else {
+          this.$message.warning(resp.data.message);
+        }
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
 .editor-btn {
-    margin-top: 20px;
-    margin-bottom: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 .article-select {
-    margin-top: 10px;
-    margin-bottom: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 .article-title {
-    margin-top: 10px;
-    margin-bottom: 16px;
+  margin-top: 10px;
+  margin-bottom: 16px;
 }
 </style>
