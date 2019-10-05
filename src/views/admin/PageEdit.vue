a968dcdc94e8fa12dfae62a2c38f7eceff207e55
smallsixFight <smallsix.fight@gmail.com>
2019-10-05 23:49:57 +0800

    重构页面编辑页和系统设置信息页；

--- src/views/admin/PageEdit.vue
+++ src/views/admin/PageEdit.vue
@@ -1,120 +1,115 @@
 <template>
-    <div v-loading="loading">
-        <el-form :model="pageForm" :rules="rules" ref="pageForm">
-            <el-card class="page-header">
-                <span>文章编辑</span>
-            </el-card>
-            <el-card>
-                <el-form-item>
-                    <el-input v-model="pageForm.slug" placeholder="自定义路径 例: example" size="small"></el-input>
-                </el-form-item>
-                <el-form-item prop="title">
-                    <el-input placeholder="输入文章标题" v-model="pageForm.title" class="article-title"></el-input>
-                </el-form-item>
-                <el-form-item prop="content">
-                    <mavon-editor v-model="pageForm.content" ref="md" @change="change" style="min-height: 600px;" />
-                </el-form-item>
-                <el-form-item>
-                    <div style="float: right;">
-                        <el-button class="editor-btn" size="small" type="warning" @click="submit('draft')">保存草稿</el-button>
-                        <el-button class="editor-btn" size="small" type="primary" @click="submit('publish')">发布</el-button>
-                    </div>
-                </el-form-item>
-            </el-card>
-        </el-form>
-    </div>
+  <div v-loading="loading">
+    <el-form :model="pageForm">
+      <el-card class="page-header">
+        <span>自定义页面编辑</span>
+      </el-card>
+      <el-card>
+        <el-form-item prop="title">
+          <el-input placeholder="输入标题" v-model="pageForm.title" class="page-title"></el-input>
+        </el-form-item>
+        <el-form-item prop="content">
+          <markdownEdit
+            :key="pageForm.id"
+            :val="pageForm.content"
+            v-on:change="handleContentChange"
+          ></markdownEdit>
+        </el-form-item>
+        <el-form-item>
+          <div style="float: right;">
+            <el-button class="editor-btn" size="small" type="warning" @click="submit(0)">存为草稿</el-button>
+            <el-button class="editor-btn" size="small" type="primary" @click="submit(1)">发布</el-button>
+          </div>
+        </el-form-item>
+      </el-card>
+    </el-form>
+  </div>
 </template>
 
 <script>
-import { mavonEditor } from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
-import { stringify } from 'qs'
+import markdownEdit from "../components/MarkdownEdit";
 export default {
-    name: "PageEdit",
-    data() {
-        return {
-            loading: false,
-            pageForm: {
-                content: '',
-                title: '',
-                slug: ''
-            },
-            rules: {
-                 title: [
-                    { required: true, message: '请输入页面标题', trigger: 'blur' }
-                ],
-                content: [
-                    { required: true, message: '请输入页面内容', trigger: 'blur' }
-                ]
-            },
-            html:'',
-        }
-    },
-    components: {
-        mavonEditor
-    },
-    created: function() {
-        const cid = this.$route.query.cid
-        if (cid) {
-            this.getInitalizeData(cid);
-        }
-    },
-    methods: {
-        submit(status) {
-            this.$refs['pageForm'].validate((valid) => {
-                if (!valid)
-                    return false
-                this.loading = true
-                this.pageForm.status = status
-                this.$axios.post(this.HOST + "/admin/api/addOrUpdatePage", {
-                    ...this.pageForm
-                }).then(response => {
-                    if (response.data.code === 0) {
-                        this.dialogFormVisible = false
-                        this.$router.replace('/admin/pageList')
-                        this.$message.success(response.data.message)
-                    } else {
-                        this.$message.warning(response.data.message)
-                    }
-                }).finally( () => {
-                    this.loading = false
-                })
-            })
-        },
-        getInitalizeData(cid) {
-            this.loading = true
-            let params = {
-                "cid": cid
-            }
-            this.$axios.get(this.HOST + `/admin/api/pageEditInitData?${stringify(params)}`)
-            .then(response => {
-                if (response.data.code === 0) {
-                    const resp = response.data.data
-                    this.pageForm.cid = resp.cid
-                    this.pageForm.title = resp.title
-                    this.pageForm.content = resp.content
-                    this.pageForm.slug = resp.slug || ''
-                } else {
-                    this.$message.warning(response.data.message)
-                }
-            }).finally( () => {
-                this.loading = false
-            })
-        },
-        change(value, render) {
-            this.html = render;
-        }
+  name: "PageEdit",
+  data() {
+    return {
+      loading: false,
+      pageForm: {
+        content: "",
+        title: "",
+      },
+    };
+  },
+  components: {
+    markdownEdit,
+  },
+  created: function() {
+    const id = this.$route.query.id;
+    if (id && id !== 0) {
+      this.loading = true;
+      this.$axios
+        .get(this.HOST + "/admin/custom/page/info/" + id)
+        .then(resp => {
+          if (resp.data.success) {
+            this.pageForm = resp.data.data;
+          } else {
+            this.$message.warning(resp.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
-}
+  },
+  methods: {
+      handleContentChange(val) {
+      this.pageForm.content = val;
+    },
+    async submit(status) {
+    if (this.pageForm.title.trim() === "") {
+        this.$message.warning("请输入标题");
+        return;
+      }
+      if (this.pageForm.content.trim() === "") {
+        this.$message.warning("请输入内容");
+        return;
+      }
+
+      this.pageForm.status = status;
+      try {
+        this.loading = true;
+        let resp;
+        if (this.pageForm.id && this.pageForm.id !== '0') {
+          delete this.pageForm.create_time;
+          delete this.pageForm.modify_time;
+          resp = await this.$axios.put(this.HOST + "/admin/custom/page/update", {
+            ...this.pageForm
+          });
+        } else {
+          resp = await this.$axios.post(this.HOST + "/admin/custom/page/add", {
+            ...this.pageForm
+          });
+        }
+        if (resp.data.success) {
+          this.$message.success(resp.data.message);
+          this.$router.replace("/admin/pageList");
+        } else {
+          this.$message.warning(resp.data.message);
+        }
+      } finally {
+        this.loading = false;
+      }
+    },
+  }
+};
 </script>
 
 <style scoped>
 .editor-btn {
-    margin-top: 20px;
-    margin-bottom: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
-.article-title {
-    margin-top: 10px;
-    margin-bottom: 16px;
+.page-title {
+  margin-top: 10px;
+  margin-bottom: 16px;
 }
 </style>
--- src/views/admin/SysSetting.vue
+++ src/views/admin/SysSetting.vue
@@ -1,226 +1,263 @@
 <template>
-    <div>
-        <el-card class="page-header">
-            <span>系统设置</span>
-        </el-card>
-        <el-card>
-            <el-tabs v-model="selectTag">
-                <el-tab-pane label="基本设置" name="basicSetting">
-                    <el-form :model="basicSettingForm" label-width="80px" style="width: 40%; margin-top: 20px;">
-                        <el-form-item label="博客名称">
-                            <el-input v-model="basicSettingForm.blogName" placeholder="博客名称" size="small"></el-input>
-                        </el-form-item>
-                        <el-form-item label="用户名称">
-                            <el-input v-model="basicSettingForm.username" size="small"></el-input>
-                        </el-form-item>
-                        <el-form-item label="显示昵称">
-                            <el-input v-model="basicSettingForm.screenName" size="small"></el-input>
-                        </el-form-item>
-                        <el-form-item label="域名">
-                            <el-input v-model="basicSettingForm.homeUrl" size="small"></el-input>
-                        </el-form-item>
-                        <el-form-item label="邮箱">
-                            <el-input v-model="basicSettingForm.email" size="small"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button size="small" type="primary" @click="submitBasicForm" :loading="loading">保存</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-tab-pane>
-                <el-tab-pane label="附件设置" name="acctchmentSetting">
-                    <el-form :model="attachSettingForm" label-width="80px" style="width: 40%; margin-top: 20px;">
-                        <el-form-item label="存储位置">
-                            <el-radio-group v-model="attachSettingForm.attachSavePlace" @change="handleRadioChange">
-                                <el-radio label="local">本地</el-radio>
-                                <el-radio label="qiNiuYun">七牛云</el-radio>
-                            </el-radio-group>
-                        </el-form-item>
-                        <div v-if="qiNiuYunOptions">
-                            <el-form-item label="域名">
-                                <el-input v-model="attachSettingForm.qiNiuYunDomain" size="small"></el-input>
-                            </el-form-item>
-                            <el-form-item label="AccessKey">
-                                <el-input v-model="attachSettingForm.qiNiuYunAccessKey" size="small"></el-input>
-                            </el-form-item>
-                            <el-form-item label="SecretKey">
-                                <el-input v-model="attachSettingForm.qiNiuYunSecretKey" size="small"></el-input>
-                            </el-form-item>
-                            <el-form-item label="Bucket">
-                                <el-input v-model="attachSettingForm.qiNiuYunBucket" size="small"></el-input>
-                            </el-form-item>
-                        </div>
-                        <el-form-item>
-                            <el-button size="small" type="primary" @click="submitAttachSettingForm" :loading="loading">保存</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-tab-pane>
-                <el-tab-pane label="密码修改" name="passwordChange">
-                    <el-form :model="passwordForm" :rules="passwordRule" ref="passwordForm" label-width="80px" style="width: 40%; margin-top: 20px;">
-                        <el-form-item label="原密码" prop="oldPass">
-                            <el-input type="password" size="small" v-model="passwordForm.oldPass"></el-input>
-                        </el-form-item>
-                        <el-form-item label="新密码" prop="newPass">
-                            <el-input type="password" size="small" v-model="passwordForm.newPass"></el-input>
-                        </el-form-item>
-                        <el-form-item label="确认密码" prop="checkPass">
-                            <el-input type="password" size="small" v-model="passwordForm.checkPass"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button size="small" type="primary" @click="changePass" :loading="loading">修改</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-tab-pane>
-            </el-tabs>
-        </el-card>
-    </div>
+  <div>
+    <el-card class="page-header">
+      <span>系统设置</span>
+    </el-card>
+    <el-card>
+      <el-tabs v-model="select_tag" @tab-click="handleTagClick">
+        <el-tab-pane label="博客设置" name="blog_setting">
+          <el-form :model="blog_setting" label-width="80px" style="width: 40%; margin-top: 20px;">
+            <el-form-item label="博客名称">
+              <el-input v-model="blog_setting.blog_name" placeholder="请输入博客名称" size="small"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                size="small"
+                type="success"
+                @click="saveBlogInfo(1)"
+                :loading="loading"
+              >临时保存</el-button>
+              <el-button
+                size="small"
+                type="primary"
+                @click="saveBlogInfo(2)"
+                :loading="loading"
+              >永久保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="七牛云设置" name="qiniu_setting">
+          <el-form :model="qiniu_setting" label-width="80px" style="width: 40%; margin-top: 20px;">
+            <div>
+              <el-form-item label="绑定域名">
+                <el-input placeholder="请输入在七牛云绑定的域名" v-model="qiniu_setting.domain" size="small"></el-input>
+              </el-form-item>
+              <el-form-item label="AccessKey">
+                <el-input
+                  placeholder="请输入 accessKey"
+                  v-model="qiniu_setting.access_key"
+                  size="small"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="SecretKey">
+                <el-input
+                  type="password"
+                  v-model="qiniu_setting.secret_key"
+                  placeholder="请输入 secretKey"
+                  size="small"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="Bucket">
+                <el-input placeholder="请输入 bucket" v-model="qiniu_setting.bucket" size="small"></el-input>
+              </el-form-item>
+              <el-form-item label="存储地址">
+                <el-input
+                  placeholder="e.g. http://upload.qiniup.com"
+                  v-model="qiniu_setting.upload_url"
+                  size="small"
+                ></el-input>
+              </el-form-item>
+            </div>
+            <el-form-item>
+              <el-button
+                size="small"
+                type="success"
+                @click="saveQiniuInfo(1)"
+                :loading="loading"
+              >临时保存</el-button>
+              <el-button
+                size="small"
+                type="primary"
+                @click="saveQiniuInfo(2)"
+                :loading="loading"
+              >永久保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="用户设置" name="user_setting">
+          <el-form :model="user_setting" label-width="80px" style="width: 40%; margin-top: 20px;">
+            <el-form-item label="名称">
+              <el-input placeholder="请输入用户名称" size="small" v-model="user_setting.username"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input placeholder="请输入邮箱" size="small" v-model="user_setting.e_mail"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button size="small" type="primary" @click="saveUserInfo" :loading="loading">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="密码设置" name="pwd_setting">
+          <el-form :model="pwd_setting" label-width="80px" style="width: 40%; margin-top: 20px;">
+            <el-form-item label="旧密码">
+              <el-input placeholder="请输入旧密码" type="password" size="small" v-model="pwd_setting.old_pwd"></el-input>
+            </el-form-item>
+            <el-form-item label="新密码">
+              <el-input placeholder="请输入新密码" type="password" size="small" v-model="pwd_setting.new_pwd"></el-input>
+            </el-form-item>
+            <el-form-item label="重复密码">
+              <el-input placeholder="请再次输入新密码" type="password" size="small" v-model="pwd_setting.confirm_pwd"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button size="small" type="primary" @click="updatePwd" :loading="loading">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        let validateOldPass = (rule, value, callback) => {
-            if (value === '') {
-                callback(new Error('请输入密码'));
-            }
-            callback()
-        };
-        let validateNewPass = (rule, value, callback) => {
-            if (value === '') {
-                callback(new Error('请输入新的密码'));
-            }
-            callback()
-        };
-        let validateCheckPass = (rule, value, callback) => {
-            if (value === '') {
-            callback(new Error('请再次输入密码'));
-            } else if (value !== this.passwordForm.newPass) {
-                callback(new Error('两次输入密码不一致!'));
-            }
-            callback()
-        };
-        return {
-            loading: false,
-            qiNiuYunOptions: false,
-            selectTag: 'basicSetting',
-            basicSettingForm: {
-                blogName: '',
-                username: '',
-                screenName: '',
-                email: '',
-                homeUrl: ''
-            },
-            attachSettingForm: {
-                attachSavePlace: 'local',
-                qiNiuYunDomain: '',
-                qiNiuYunAccessKey: '',
-                qiNiuYunSecretKey: '',
-                qiNiuYunBucket: ''
-            },
-            passwordForm: {
-                oldPass: '',
-                newPass: '',
-                checkPass: ''
-            },
-            passwordRule: {
-                oldPass: [
-                    { validator: validateOldPass, trigger: 'blur' }
-                ],
-                newPass: [
-                    { validator: validateNewPass, trigger: 'blur' }
-                ],
-                checkPass: [
-                    { validator: validateCheckPass, trigger: 'blur' }
-                ],
-            }
-        }
-    },
-    created: function() {
-        this.loading = true
-        this.$axios.get(this.HOST + `/admin/api/sysSettingInfo`)
+  data() {
+    return {
+      loading: false,
+      select_tag: "blog_setting",
+      blog_setting: {
+        blog_name: ""
+      },
+      qiniu_setting: {
+        domain: "",
+        access_key: "",
+        secret_key: "",
+        bucket: "",
+        upload_url: ""
+      },
+      user_setting: {
+        username: "",
+        e_mail: ""
+      },
+      pwd_setting: {
+        old_pwd: "",
+        new_pwd: "",
+        confirm_pwd: ""
+      }
+    };
+  },
+
+  created: function() {
+    this.getInitData();
+  },
+
+  methods: {
+    getInitData() {
+      this.loading = true;
+      this.$axios
+        .get(this.HOST + `/admin/sys/config`)
         .then(response => {
-            if (response.data.code === 0) {
-                const resp = response.data.data
-                if (resp.blogOptions) {
-                    this.attachSettingForm.qiNiuYunDomain = resp.blogOptions.qiNiuYunDomain || '';
-                    this.attachSettingForm.qiNiuYunAccessKey = resp.blogOptions.qiNiuYunAccessKey || '';
-                    this.attachSettingForm.qiNiuYunSecretKey = resp.blogOptions.qiNiuYunSecretKey || '';
-                    this.attachSettingForm.qiNiuYunBucket = resp.blogOptions.qiNiuYunBucket || '';
-                    this.attachSettingForm.attachSavePlace = resp.blogOptions.attachSavePlace;
-                }
-                this.basicSettingForm.blogName = resp.users.blogName;
-                this.basicSettingForm.username = resp.users.username;
-                this.basicSettingForm.screenName = resp.users.screenName;
-                this.basicSettingForm.email = resp.users.email;
-                this.basicSettingForm.homeUrl = resp.users.homeUrl;
-                this.handleRadioChange(resp.blogOptions.attachSavePlace)
-            } else {
-                this.$message.warning(response.data.message)
-            }
-        }).finally( () => {
-            this.loading = false
+          if (response.data.success) {
+            const data = response.data.data;
+            this.blog_setting = data.blog_info;
+            this.qiniu_setting = data.qiniu;
+            this.user_setting = data.user_config;
+          } else {
+            this.$message.warning(response.data.message);
+          }
         })
+        .finally(() => {
+          this.loading = false;
+        });
     },
-    methods: {
-        changePass() {
-            this.$refs['passwordForm'].validate((valid) => {
-                if (!valid)
-                    return false
-                this.loading = true
-                this.$axios.post(this.HOST + "/admin/api/changePassword", {
-                ...this.passwordForm
-                }).then(response => {
-                    if (response.data.code === 0) {
-                        this.$refs['passwordForm'].resetFields();
-                        this.$message.success(response.data.message)
-                    } else {
-                        this.$message.warning(response.data.message)
-                    }
-                }).finally( () => {
-                    this.loading = false
-                })
-            })
-        },
-        submitAttachSettingForm() {
-            this.loading = true
-            this.$axios.post(this.HOST + "/admin/api/updateAttachSetting", {
-                ...this.attachSettingForm
-            }).then(response => {
-                if (response.data.code === 0) {
-                    this.dialogFormVisible = false
-                    this.$message.success(response.data.message)
-                } else {
-                    this.$message.warning(response.data.message)
-                }
-            }).finally( () => {
-                this.loading = false
-            })
-        },
-        submitBasicForm() {
-            this.loading = true
-            this.$axios.post(this.HOST + "/admin/api/updateBasicSetting", {
-                ...this.basicSettingForm
-            }).then(response => {
-                if (response.data.code === 0) {
-                    this.dialogFormVisible = false
-                    this.$message.success(response.data.message)
-                } else {
-                    this.$message.warning(response.data.message)
-                }
-            }).finally( () => {
-                this.loading = false
-            })
-        },
 
-        handleRadioChange(val) {
-            this.attachSettingForm.attachSavePlace = val
-            if (val === 'local') {
-                this.qiNiuYunOptions = false
-            } else if (val === 'qiNiuYun') {
-                this.qiNiuYunOptions = true
-            }
-            
-            
-        }
+    handleTagClick() {
+      if (this.select_tag === "pwd_setting") {
+        this.pwd_setting = {
+          old_pwd: "",
+          new_pwd: "",
+          confirm_pwd: ""
+        };
+      }
+    },
+
+    saveBlogInfo(typ) {
+      this.loading = true;
+      this.$axios
+        .put(this.HOST + `/admin/sys/blog/info/update`, {
+          ...this.blog_setting,
+          typ
+        })
+        .then(response => {
+          if (response.data.success) {
+            this.$message.success(response.data.message);
+            this.getInitData();
+          } else {
+            this.$message.warning(response.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+
+    saveQiniuInfo(typ) {
+      this.loading = true;
+      this.$axios
+        .put(this.HOST + `/admin/sys/qiniu/setting/update`, {
+          ...this.qiniu_setting,
+          typ
+        })
+        .then(response => {
+          if (response.data.success) {
+            this.$message.success(response.data.message);
+            this.getInitData();
+          } else {
+            this.$message.warning(response.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+
+    saveUserInfo() {
+      this.loading = true;
+      this.$axios
+        .put(this.HOST + `/admin/user/info/update`, {
+          ...this.user_setting
+        })
+        .then(response => {
+          if (response.data.success) {
+            this.$message.success(response.data.message);
+            sessionStorage.setItem("ms_username", this.user_setting.username);
+            this.getInitData();
+          } else {
+            this.$message.warning(response.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+
+    updatePwd() {
+      if (this.pwd_setting.old_pwd.trim() === '') {
+        this.$message.warning('请输入旧密码')
+        return
+      }
+      if (this.pwd_setting.new_pwd.trim() === '' || this.pwd_setting.confirm_pwd.trim() === '') {
+        this.$message.warning('请输入新密码/重复密码')
+        return
+      }
+      if (this.pwd_setting.new_pwd.trim() !== this.pwd_setting.confirm_pwd.trim()) {
+        this.$message.warning('两次密码输入不一致')
+        return
+      }
+      this.loading = true;
+      this.$axios.post(this.HOST + `/admin/user/pwd/update`, {
+        ...this.pwd_setting,
+      })
+      .then(response => {
+          if (response.data.success) {
+            this.$message.success(response.data.message);
+            this.handleTagClick();
+          } else {
+            this.$message.warning(response.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
-}
+  }
+};
 </script>
-
