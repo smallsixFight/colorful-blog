ff5b8281e179fd27faefe7c7383687c38a96bc91
smallsixFight <smallsix.fight@gmail.com>
2019-10-27 23:35:07 +0800

    commit:
    - 修改首页信息接口及展示调整；
    - 修改文章列表接口及修正分页页码问题；
    - 调整友链信息获取接口；

--- src/views/colorfulStory/Header.vue
+++ src/views/colorfulStory/Header.vue
@@ -1,80 +1,89 @@
 <template>
-    <header id="header" class="clearfix" v-loading="loading">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="logo">
-                    <div class="header-logo">
-                        <router-link to="/">
-                            <span class="b">Start</span>
-                        </router-link>
-                        <a target="_blank" href="https://gitHub.com/smallsixFight">
-                            <span class="b">GitHub</span>
-                        </a>
-                        <router-link to="/read">
-                            <span class="b">Read</span>
-                        </router-link>
-                        <router-link to="/archive">
-                            <span class="b">Archive</span>
-                        </router-link>
-                        <router-link to="/link" v-if="hasLink">
-                            <span class="b">Friend</span>
-                        </router-link>
-                        <span v-for="customPage in customPageList" :key='customPage.cid'>
-                            <router-link :to="'/page/' + customPage.cid" :index="customPage.cid">
-                                <span class="b">{{ customPage.slug || customPage.title }}</span>
-                            </router-link>
-                        </span>
-                        
-                    </div>
-                    <div id="menu-page">
-                        <!-- <@commonTag method="menus">
+  <header id="header" class="clearfix" v-loading="loading">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="logo">
+          <div class="header-logo">
+            <router-link to="/">
+              <span class="b">首页</span>
+            </router-link>
+            <a target="_blank" :href="github_url">
+              <span class="b">Gitub</span>
+            </a>
+            <router-link to="/read">
+              <span class="b">读书</span>
+            </router-link>
+            <router-link to="/archive">
+              <span class="b">归档</span>
+            </router-link>
+            <router-link to="/link" v-if="has_link">
+              <span class="b">友链</span>
+            </router-link>
+            <span v-for="pageInfo in page_list" :key="pageInfo.id">
+              <router-link :to="'/page/' + pageInfo.id" :index="pageInfo.id">
+                <span class="b">{{ pageInfo.slug || pageInfo.title }}</span>
+              </router-link>
+            </span>
+          </div>
+          <div id="menu-page">
+            <!-- <@commonTag method="menus">
                             <#list menus?sort_by('menuSort') as menu>
                                 <a href="${menu.menuUrl}"><li>${menu.menuName}</li></a>
                             </#list>
-                        </@commonTag> -->
-                    </div>
-                    <div id="search-box">
-                        <form id="search" method="post" action="./" role="search">
-                            <input autocomplete="off" type="text" name="s" id="menu-search" placeholder="Type something~" />
-                        </form>
-                    </div>
-                </div>
-            </div>
+            </@commonTag>-->
+          </div>
+          <div id="search-box">
+            <form id="search" method="post" action="./" role="search">
+              <input
+                autocomplete="off"
+                type="text"
+                name="s"
+                id="menu-search"
+                placeholder="Type something~"
+              />
+            </form>
+          </div>
         </div>
-    </header>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            loading: false,
-            hasLink: false,
-            customPageList: []
-        }
-    },
-    created: function() {
-        this.getInitData();
-    },
-    methods: {
-        getInitData() {
-            this.loading = true
-            this.$axios.get(this.HOST + `/api/initData`)
-            .then(response => {
-                if (response.data.code === 0) {
-                    const resp = response.data.data
-                    document.title = resp.blogName + "’ Blog"
-                    this.hasLink = resp.hasPublishLink
-                    this.customPageList = resp.pageList
-                } else {
-                    this.$message.warning(response.data.message)
-                }
-            }).finally( () => {
-                this.loading = false
-            })
-        },
+  data() {
+    return {
+      loading: false,
+      has_link: false,
+      github_url: "https://github.com",
+      page_list: []
+    };
+  },
+  created: function() {
+    this.getInitData();
+  },
+  methods: {
+    getInitData() {
+      this.loading = true;
+      this.$axios
+        .get(this.HOST + `/visitor/index/info`)
+        .then(response => {
+          if (response.data.success) {
+            const resp = response.data.data;
+            document.title = resp.blog_name ? resp.blog_name : "Colorful Blog";
+            this.has_link = resp.has_link;
+            this.github_url = resp.github_url && resp.github_url;
+            this.page_list = resp.page_list;
+          } else {
+            this.$message.warning(response.data.message);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
-}
+  }
+};
 </script>
 
 
--- src/views/colorfulStory/Link.vue
+++ src/views/colorfulStory/Link.vue
@@ -1,44 +1,43 @@
 <template>
-    <div class="row" v-loading="loading">
-        <div id="main" class="col-12 clearfix" role="main">
-            <article class="posti">
-                <h1 style="text-align:right;" class="post-title">Friends</h1>
-                <div class="post-content" itemprop="pageBody">
-                    <ul v-if="linkList.length > 0">
-                        <li v-for="link in linkList" :key="link.id">
-                            <a target="_blank" :href="link.content">{{link.owner}}</a>
-                        </li>
-                    </ul>
-                    <span v-if="linkList.length === 0">暂无友链</span>
-                </div>
-            </article>
+  <div class="row" v-loading="loading">
+    <div id="main" class="col-12 clearfix" role="main">
+      <article class="posti">
+        <h1 style="text-align:right;" class="post-title">Friends</h1>
+        <div class="post-content" itemprop="pageBody">
+          <ul v-if="link_list.length > 0">
+            <li v-for="link in link_list" :key="link.id">
+              <a target="_blank" :title="link.description" :href="link.url">{{link.owner}}</a>
+            </li>
+          </ul>
+          <span v-if="!link_list.length">暂无友链</span>
         </div>
+      </article>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            loading: false,
-            linkList: []
+  data() {
+    return {
+      loading: false,
+      link_list: []
+    };
+  },
+  created: function() {
+    this.loading = true;
+    this.$axios
+      .get(this.HOST + `/visitor/link/list`)
+      .then(resp => {
+        if (resp.data.success) {
+          this.link_list = resp.data.data;
+        } else {
+          // this.$router.replace("/404")
+          this.$message.error(response.data.message);
         }
-    },
-    created: function() {
-        this.loading = true
-        this.$axios.get(this.HOST + `/api/getLinkList`)
-            .then(response => {
-                if (response.data.code === 0) {
-                    this.linkList = response.data.data
-                } else if(response.data.code === 1) {
-                    this.$router.replace("/")
-                    this.$message.warning(response.data.message)
-                } else {
-                    this.$router.replace("/404")
-                    this.$message.error(response.data.message)
-                }
-            }).finally( () => {
-                this.loading = false
-        })
-    }
-}
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+};
 </script>
